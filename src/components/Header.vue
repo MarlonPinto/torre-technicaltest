b7af5e7fa22e6e233e78a32c96c6bebe03864753
MarlonPinto <marlonpinto.multi@gmail.com>
2021-06-23 20:00:52 -0500

    First Commit Test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h1>TITLE</h1>
+        <h3>This is a commit test</h3>
     </div>
 </template>
 <script>
     export default {
       name: 'Header'
-        
+
     }
 </script>
